bb7e2a8912db786ae0224c76867cfafc4c5eae54
Kson <igor.kheison@gmail.com>
2021-01-29 01:23:13 +0200

    spawner

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField] float speed = 9f;
+    private void Update()
+    {
+        this.gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] spawnables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 2);
+    }
+
+    void SpawnObjects()
+    {
+        int randomIndex = Random.Range(0, spawnables.Length);
+        Vector3 spawnPos = new Vector3(15.5f, Random.Range(-4, 7), 0);
+        Instantiate(spawnables[randomIndex], spawnPos, Quaternion.identity);
+    }
+}
