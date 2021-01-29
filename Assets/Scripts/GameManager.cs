a437713266dddf44b7cc8dc34c0b96c08ef85b6b
Kson <igor.kheison@gmail.com>
2021-01-30 01:43:03 +0200

    Stage one walkthrough

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,23 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject puzzlePopupPanel;
     [SerializeField] Image puzzlePieceToRemember;
     [SerializeField] PuzzlePanel gates;
-    [SerializeField] Sprite otherPP;
 
     SpawnManager spawner;
     PlayerMovement player;
     MoveBackground[] moveBackground;
     Coroutine _showPanel;
+    Coroutine _guessingTime;
+    Sprite correctPP;
 
-    //int stageCount = 1;
+    int stageCount = 1;
+    int _winningNumber;
     float timeToRemember = 2f;
+    int indexToStoreSprite;
+
 
     public bool shouldSpawn = true;
+    public bool timeToGuess = false;
+    public bool wonThisRound = false;
     public int HighScore = 0;
     public static GameManager Instance;
 
@@ -31,7 +37,11 @@ public class GameManager : MonoBehaviour
         player = GameObject.FindObjectOfType<PlayerMovement>();
         moveBackground = FindObjectsOfType<MoveBackground>();
         Debug.Log(puzzlePieceToRemember.sprite.name);
-        puzzlePieceToRemember.sprite = otherPP;
+    }
+
+    private void Start()
+    {
+        SavePref.LoadPrefs();
     }
 
     // Update is called once per frame
@@ -40,13 +50,27 @@ public class GameManager : MonoBehaviour
         if (HighScore < player.CurrentScore())
         {
             HighScore = player.CurrentScore();
+            SavePref.SavePrefs();
         }
+
+        if (timeToGuess == true)
+        {
+            StoreAnswer();
+        }
+
+        if (wonThisRound == true)
+            NextStage();
+        else
+            GameOver();
     }
 
     public void StartStage()
     {
+        indexToStoreSprite = Random.Range(0, 3);
+        var thisRoundsSprite = stage[stageCount - 1].sprites[indexToStoreSprite];
+        puzzlePieceToRemember.sprite = thisRoundsSprite;
         //Start coroutine show panel
-        if(_showPanel != null) { StopCoroutine(_showPanel); }
+        if (_showPanel != null) { StopCoroutine(_showPanel); }
         else { _showPanel = StartCoroutine(ShowPanel()); }
         // Background starts moving
         foreach (var image in moveBackground)
@@ -60,16 +84,35 @@ public class GameManager : MonoBehaviour
         // Spawning should stop and be destroyed
         spawner.Spawner(false);
         spawner.DestroyAllSpawnables();
-        // Gates appear
-        gates.ShowGate();
         // Player needs to press qwer to choose correct gate, if wrong game over
         // if right, SO stage advance
+        if (_guessingTime != null) { StopCoroutine(_guessingTime); }
+        else { _guessingTime = StartCoroutine(GuessTime()); }
     }
 
     void GameOver()
     {
         // Panel pops out with restart or quit to menu
         // Score of collected items should be kept as high score
+    }
+    
+    void NextStage()
+    {
+        Debug.Log("YOU WON");
+        /*// Advance stage
+        stageCount += 1;
+        // Reset everything
+        StartStage();*/
+    }
+
+    void StoreAnswer()
+    {
+        var input = Input.inputString;
+        if(input == _winningNumber.ToString())
+        {
+            wonThisRound = true;
+            Debug.Log("You won");
+        }
     }
 
     IEnumerator ShowPanel()
@@ -78,11 +121,25 @@ public class GameManager : MonoBehaviour
         puzzlePopupPanel.SetActive(true);
         yield return new WaitForSecondsRealtime(timeToRemember);
         puzzlePopupPanel.SetActive(false);
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(0.5f);
         // After one seconds the player can move and stuff will start spawning
         player.SetKinematic(false);
         // Spawning should start
         spawner.Spawner(true);
+        _winningNumber = gates.GetAndSetWinner(indexToStoreSprite, stageCount);
+        Debug.Log(_winningNumber);
+    }
+
+    IEnumerator GuessTime()
+    {
+        gates.gameObject.SetActive(true);
+        gates.ShowGate();
+        timeToGuess = true;
+        yield return new WaitForSecondsRealtime(5f);
+
+        gates.HideGate();
+        yield return new WaitForSecondsRealtime(0.5f);
+        gates.gameObject.SetActive(false);
     }
 
     private void Singleton()
--- Assets/Scripts/PuzzlePanel.cs
+++ Assets/Scripts/PuzzlePanel.cs
@@ -6,21 +6,46 @@ using UnityEngine.UI;
 public class PuzzlePanel : MonoBehaviour
 {
     public Animator anim;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
+    [SerializeField] List<SO_Stage> stages;
+    [SerializeField] SpriteRenderer gateQsprite;
+    [SerializeField] SpriteRenderer gateWsprite;
+    [SerializeField] SpriteRenderer gateEsprite;
 
     public void ShowGate() => anim.SetTrigger("ShowGate");
     public void HideGate() => anim.SetTrigger("HideGate");
 
-    public void SetNewSetOfPuzzles()
+    public int GetAndSetWinner(int winningIndex, int stageNumber)
     {
-        Image[] spritesInChildren = GetComponentsInChildren<Image>();
-        foreach (Image sprite in spritesInChildren)
-            Debug.Log(sprite.name);
+        Debug.Log("GetSetWinnerRan");
+        int placeA;
+        int placeB;
+        if(winningIndex > 1)
+        {
+            placeA = 1;
+            placeB = 0;
+            gateQsprite.sprite = stages[stageNumber - 1].sprites[winningIndex];
+            gateWsprite.sprite = stages[stageNumber - 1].sprites[placeA];
+            gateEsprite.sprite = stages[stageNumber - 1].sprites[placeB];
+            return 1;
+        }
+        else if (winningIndex == 1)
+        {
+            placeA = 0;
+            placeB = 2;
+            gateWsprite.sprite = stages[stageNumber - 1].sprites[winningIndex];
+            gateQsprite.sprite = stages[stageNumber - 1].sprites[placeA];
+            gateEsprite.sprite = stages[stageNumber - 1].sprites[placeB];
+            return 2;
+        }
+        else
+        {
+            placeA = 1;
+            placeB = 2;
+            gateEsprite.sprite = stages[stageNumber - 1].sprites[winningIndex];
+            gateQsprite.sprite = stages[stageNumber - 1].sprites[placeB];
+            gateWsprite.sprite = stages[stageNumber - 1].sprites[placeA];
+            return 3;
+        }  
     }
 
 }
